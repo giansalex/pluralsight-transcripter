91d156955599de7410b6e78f20441cfe60640750
Giancarlos Salas <giansalex@gmail.com>
2019-06-10 16:53:12 -0500

    download all transcripters in first time

--- service/transcript.go
+++ service/transcript.go
@@ -3,16 +3,19 @@ package service
 import (
 	"fmt"
 
+	"github.com/giansalex/pluralsight-transcript/model"
 	"github.com/giansalex/pluralsight-transcript/repository"
 )
 
+// DefaultTranscriptService implementation TranscriptService
 type DefaultTranscriptService struct {
 	courses     repository.CourseRepository
 	transcripts repository.TranscriptRepository
-	api         ApiService
+	api         APIService
 }
 
-func NewTranscriptService(courses repository.CourseRepository, transcripts repository.TranscriptRepository, api ApiService) *DefaultTranscriptService {
+// NewTranscriptService instance creator
+func NewTranscriptService(courses repository.CourseRepository, transcripts repository.TranscriptRepository, api APIService) *DefaultTranscriptService {
 	return &DefaultTranscriptService{
 		courses:     courses,
 		transcripts: transcripts,
@@ -20,19 +23,26 @@ func NewTranscriptService(courses repository.CourseRepository, transcripts repos
 	}
 }
 
+// Transcript translate to specify language
 func (service *DefaultTranscriptService) Transcript(lang string) error {
 	courses := service.courses.List()
+	transcripts := []*model.CourseTranscript{}
 
-	service.transcripts.ClearAll()
 	for _, course := range courses {
-		fmt.Println("Transcript:", course.Title.String)
+		fmt.Println("Download Transcript:", course.Title.String)
 		courseTranscript, err := service.api.GetByCourse(course.Name.String, lang)
 
 		if err != nil {
 			return err
 		}
 
-		service.transcripts.Save(courseTranscript)
+		transcripts = append(transcripts, courseTranscript)
+	}
+
+	service.transcripts.ClearAll()
+	for index, transcript := range transcripts {
+		fmt.Println("Save Transcript:", courses[index].Title.String)
+		service.transcripts.Save(transcript)
 	}
 
 	return nil
